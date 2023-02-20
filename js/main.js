0e5059eba5b389295b3817133af3bee7b1fb2221
LeoSallu <L.sallustio99@gmail.com>
2023-02-20 15:35:03 +0100

    struttura css+variabile password js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 //PRIMO STEP
+    console.log (prompt)
     // INSERISCI NOME 
 const nomeUtente = prompt('Inserisci il tuo Nome');
+    
     //INSERISCI COGNOME
 const cognomeUtente = prompt('Inserisci il tuo Cognome');
+    
     //INSERISCI COLORE PREFERITO
 const coloreUtente = prompt('Inserisci il tuo Colore preferito');
+
+//SECONDO STEP
+
+    //CREA LA TUA NUOVA PASSWORD
+const passwordUtente = (nomeUtente + cognomeUtente + coloreUtente + 89);
+
+console.log(passwordUtente);
 
