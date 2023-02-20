b65224ed18f3947547e24a1553d1acafe273270d
LeoSallu <L.sallustio99@gmail.com>
2023-02-20 15:04:41 +0100

    struttura generale

---
+++ script/main.js
@@ -0,0 +1,8 @@
+//PRIMO STEP
+    // INSERISCI NOME 
+const nomeUtente = prompt('Inserisci il tuo Nome');
+    //INSERISCI COGNOME
+const cognomeUtente = prompt('Inserisci il tuo Cognome');
+    //INSERISCI COLORE PREFERITO
+const coloreUtente = prompt('Inserisci il tuo Colore preferito');
+
